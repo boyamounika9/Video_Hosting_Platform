625494816db720478fbc5092f36f99127dd3da2a
Jeevan Kumar <jeevankumarguduru3@gmail.com>
2026-05-10 15:28:11 +0530

    Add: Frontend files was added and tested through views in main project

--- project/project/urls.py
+++ project/project/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home,name='home'),
+    path('login/',views.login,name='login'),
+    path('playback/',views.playback,name='playback'),
+    path('profile/',views.profile,name='profile'),
+    path('register/',views.register,name='register'),
+    path('upload/',views.upload,name='upload'),
 ]
