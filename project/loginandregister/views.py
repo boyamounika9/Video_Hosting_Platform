aefe23be20ec9e807439cef8e1e993a079c1dc25
mounika <189mounika.b@gmail.com>
2026-05-11 14:55:56 +0530

    add : adding view for register in views

--- project/loginandregister/views.py
+++ project/loginandregister/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+
+
+
+def register(request):
+
+    if request.method == 'POST':
+
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        cpassword = request.POST.get('cpassword')
+
+        if password != cpassword:
+            return redirect('register')
+
+        User.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+        )
+
+        return redirect('home')
+
+    return render(request,'register.html')
+
--- project/project/settings.py
+++ project/project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
 ]
 
 MIDDLEWARE = [
@@ -48,6 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'loginandregister',
+
 ]
 
 ROOT_URLCONF = 'project.urls'
