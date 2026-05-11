fc374673db66650c1d416c51a7a8a2d75cac81dc
Jeevan Kumar <jeevankumarguduru3@gmail.com>
2026-05-11 15:15:18 +0530

    modified:   project/urls.py         new file:   videoupload/migrations/0002_videodata_thumbnail.py         modified:   videoupload/models.py         new file:   videoupload/urls.py         modified:   videoupload/views.py

--- project/videoupload/models.py
+++ project/videoupload/models.py
@@ -4,7 +4,10 @@ from django.contrib.auth.models import User
 class VideoData(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
+
     video_file = models.FileField(upload_to='videos/')
+    thumbnail = models.ImageField(upload_to='thumbnails/')
+
     upload_time = models.DateTimeField(auto_now_add=True)
     uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
--- project/videoupload/views.py
+++ project/videoupload/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import VideoData
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# @login_required
+# def VideoUpload(request):
+
+def VideoUpload(request):
+
+    if request.method == 'POST':
+
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+
+        # Get uploaded file
+        video_file = request.FILES.get('video_file')
+        thumbnail = request.FILES.get('thumbnail')
+
+        # Save into database
+        VideoData.objects.create(
+        title=title,
+        description=description,
+        video_file=video_file,
+        thumbnail=thumbnail,
+        uploaded_by=request.user
+        )
+
+        return redirect('home')
+
+    return render(request, "upload.html")
