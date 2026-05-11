55915c788e897785105f6ca1d74a48146ea0bc29
mounika <189mounika.b@gmail.com>
2026-05-11 15:30:05 +0530

    add : adding view for the login functionality in views of loginandregister app

--- project/loginandregister/views.py
+++ project/loginandregister/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+
 
 
 def home(request):
@@ -42,3 +44,16 @@ def register(request):
         return redirect('login')
 
     return render(request, 'register.html')
+
+
+def login_page(request):
+    if request.method=="POST":
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            return redirect('register')
+    return render(request,'login.html')
