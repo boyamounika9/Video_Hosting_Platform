2592e1ca8aea8c8427ccfb2efa3b67ff9fd88f75
Jeevan Kumar <jeevankumarguduru3@gmail.com>
2026-05-11 15:44:20 +0530

    mod: @login_required is added in views

--- project/videoupload/views.py
+++ project/videoupload/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .models import VideoData
 from django.contrib.auth.decorators import login_required
 
-# @login_required
+@login_required
 def VideoUpload(request):
 
     if request.method == 'POST':
