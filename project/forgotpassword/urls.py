9fae03ef217c8e60491be2a93ec274d2bf35169a
mounika <189mounika.b@gmail.com>
2026-05-12 13:26:07 +0530

    add : adding view for the forgotpassword and adding url

---
+++ project/forgotpassword/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('forgot-password/', views.forgot_password, name='forgot_password'),
+
+]
+
--- project/forgotpassword/views.py
+++ project/forgotpassword/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+def forgot_password(request):
+
+    if request.method == 'POST':
+
+        username = request.POST.get('username')
+
+        new_password = request.POST.get('new_password')
+
+        try:
+
+            user = User.objects.get(username=username)
+
+            user.set_password(new_password)
+
+            user.save()
+
+            messages.success(request,'Password changed successfully')
+
+            return redirect('login')
+
+        except User.DoesNotExist:
+
+            messages.error(request,'User not found')
+
+    return render(request,'forgot_password.html')
