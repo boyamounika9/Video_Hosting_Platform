9d9ac9823a340e8d5903533ee53f6ddb62643759
Jeevan Kumar <jeevankumarguduru3@gmail.com>
2026-05-24 18:27:03 +0530

    Project was completed 😊
    Co-authored-by: Mounika <189mounika.b@gmail.com>

--- project/project/settings.py
+++ project/project/settings.py
@@ -89,12 +89,17 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 # project/settings.py
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if env('DATABASE_URL', default=None):
+    DATABASES = {
+        'default': env.db('DATABASE_URL')
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 
